fc5f5a0f28c136e6c0663936d76078f2b5c986a0
Maria <alece.1414@gmail.com>
2023-04-16 15:35:23 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const result = [];
+
+  let {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} = options;
+
+  if(typeof str !== 'string') {
+    str = String(str);
+  }
+  if(addition !== undefined && typeof addition !== 'string') {
+    addition = String(addition);
+  }
+
+  if(!repeatTimes) {
+    repeatTimes = 1;
+  }
+
+  if(addition && !additionRepeatTimes) {
+    additionRepeatTimes = 1;
+  }
+
+  for(let i = 0; i < repeatTimes; i++) {
+    if(addition) {
+      const subArr = [];
+      for(let j = 0; j < additionRepeatTimes; j++) {
+        subArr.push(addition);
+      }
+      result.push(`${str}${subArr.join(additionSeparator || '|')}`);
+    } else {
+      result.push(str);
+    }
+  }
+
+  return result.join(separator || '+');
 }
+
 
 module.exports = {
   repeater
