4821d52d37984ea54c530c6efb3c8be4a4332a0f
Maria <alece.1414@gmail.com>
2023-04-16 20:25:22 +0300

    feat: solve 3 tasks

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,10 +10,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let res;
+
+  for(let i = 0; i < email.length; i++) {
+    if(email[i] === '@') {
+      res = email.slice(i + 1);
+    }
+  }
+  return res;
 }
+
+// console.log(getEmailDomain('prettyandsimple@example.com'))
 
 module.exports = {
   getEmailDomain
--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -8,17 +8,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @return {Number}
  *
  * @example
- * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
- * ]
+ *matrix = [
+ * [0, 1, 1, 2],
+ * [0, 5, 0, 0],
+ * [2, 0, 3, 3]
+ *]
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      if(i === 0) {
+        sum += matrix[i][j];
+      } else {
+        if(matrix[i - 1][j] !== 0) {
+          sum += matrix[i][j];
+        } 
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = {
